aac7a50aaa57d23a4e6d47d0cd54454593fc5e15
funkyfunt <valeria.brevis.5689@gmail.com>
2021-01-30 11:45:56 -0600

    v2_submittedfile

---
+++ SamplePrograms/Example.java
@@ -0,0 +1,19 @@
+a = b;
+//b = a + (foo + bar) + baz;
+//display baz;
+//display b;
+//x= !(a - b);
+//foo = new foo();
+//d= 10+1;
+//c= new c();
+//t= true;
+//f= false;
+//x= x.length;
+//foo=f.foo();
+//X = x.a(y-a,z+f);
+//z=x[1];
+//if (x < 1) s = 2;//if
+//while (x < 1) y = 1;//while
+//System.out.println(z);//print
+//x [r]= x;//arr assign
+
